9be7cf7cd77e96f909095398145fbb3dba771376
Ben Baron <ben@einsteinx2.com>
2021-11-20 18:05:18 -0500

    Prefixed stuff with "UART" to namespace for other projects

--- src/main.c
+++ src/main.c
@@ -15,21 +15,21 @@
 #include <util/delay.h>
 
 // change these to use another pin
-#define TX_PORT PORTC
-#define TX_PIN  PC7
-#define TX_DDR  DDRC
-#define TX_DDR_PIN DDC7
+#define UART_TX_PORT PORTC
+#define UART_TX_PIN  PC7
+#define UART_TX_DDR  DDRC
+#define UART_TX_DDR_PIN DDC7
 
-volatile uint16_t tx_shift_reg = 0;
+volatile uint16_t UART_tx_shift_reg = 0;
 
 void UART_tx(char character) {
-   uint16_t local_tx_shift_reg = tx_shift_reg;
+   uint16_t local_tx_shift_reg = UART_tx_shift_reg;
    //if sending the previous character is not yet finished, return
-   //transmission is finished when tx_shift_reg == 0
+   //transmission is finished when UART_tx_shift_reg == 0
    if(local_tx_shift_reg){return;}
    //fill the TX shift register witch the character to be sent and the start & stop bits (start bit (1<<0) is already 0)
    local_tx_shift_reg = (character<<1) | (1<<9); //stop bit (1<<9)
-   tx_shift_reg = local_tx_shift_reg;
+   UART_tx_shift_reg = local_tx_shift_reg;
    //start timer0 with a prescaler of 8
    TCCR0B = (1<<CS01);
 }
@@ -38,14 +38,14 @@ void UART_tx_str(char* string) {
     while( *string ) {
         UART_tx( *string++ );
         //wait until transmission is finished
-        while(tx_shift_reg);
+        while(UART_tx_shift_reg);
     }
 }
 
 void UART_init() {
    //set TX pin as output
-   TX_DDR |= (1<<TX_DDR_PIN);
-   TX_PORT |= (1<<TX_PIN);
+   UART_TX_DDR |= (1<<UART_TX_DDR_PIN);
+   UART_TX_PORT |= (1<<UART_TX_PIN);
    //set timer0 to CTC mode
    TCCR0A = (1<<WGM01);
    //enable output compare 0 A interrupt
@@ -80,18 +80,18 @@ int main(void) {
 
 //timer0 compare A match interrupt
 ISR(TIMER0_COMPA_vect) {
-   uint16_t local_tx_shift_reg = tx_shift_reg;
+   uint16_t local_tx_shift_reg = UART_tx_shift_reg;
    //output LSB of the TX shift register at the TX pin
    if (
       
       local_tx_shift_reg & 0x01 ) {
-      TX_PORT |= (1<<TX_PIN);
+      UART_TX_PORT |= (1<<UART_TX_PIN);
    } else {
-      TX_PORT &=~ (1<<TX_PIN);
+      UART_TX_PORT &=~ (1<<UART_TX_PIN);
    }
    //shift the TX shift register one bit to the right
    local_tx_shift_reg >>= 1;
-   tx_shift_reg = local_tx_shift_reg;
+   UART_tx_shift_reg = local_tx_shift_reg;
    //if the stop bit has been sent, the shift register will be 0
    //and the transmission is completed, so we can stop & reset timer0
    if (!local_tx_shift_reg) {
